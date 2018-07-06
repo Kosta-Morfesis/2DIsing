e4263e56dae0f0342d3469934babca1042905822
Kosta Morfesis <morfesisko@gmail.com>
2018-07-06 15:25:07 -0400

    added lattice test

--- 2DIsing.py
+++ 2DIsing.py
@@ -18,18 +18,29 @@ class TestSpinFlip(unittest.TestCase):
     def test_spin_flip(self):
         self.assertEqual(self.spin_new, abs(self.spin_old - 1), 'Spin did not flip!')
 
-#class TestMakeLattice(unittest.TestCase):
-#    def setUp(self):
-#        self.
+class TestMakeLattice(unittest.TestCase):
+    def setUp(self):
+        self.size = 5
+        self.lattice = make_lattice(length=self.size)
 
+    def test_default_lattice_size(self):
+        self.assertEqual(self.lattice.shape, (self.size, self.size), 'Wrong default shape')
+
+    def test_default_config(self):
+        self.assertTrue(np.array_equal(self.lattice, np.zeros((self.size, self.size))), 'Wrong default config (ground state)')
+        
 
 
 
 # spin = {0, 1}
 def spinflip(spin):
     return abs(spin - 1)
+    
+def make_lattice(length):       #ground state lattice
+    return np.zeros((length,length))
+    
 
-
+ 
 
 if __name__=='__main__':
     
